224faf4a4c6dba592c05a108cfb775ba332cb4b6
Eric Gómez Vilà <105845066+EGVPedralbes@users.noreply.github.com>
2025-03-21 11:50:39 +0100

    Initial Commit

---
+++ unity-project/Assets/Toolkits/Shop.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Shop : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("Window/UI Toolkit/Shop")]
+    public static void ShowExample()
+    {
+        Shop wnd = GetWindow<Shop>();
+        wnd.titleContent = new GUIContent("Shop");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
+    }
+}

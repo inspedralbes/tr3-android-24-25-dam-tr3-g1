f7df6320c628445f5f21ace44da40389ddc27e94
Climent Fernández <a23cliferand@inspedralbes.cat>
2025-03-27 13:31:01 +0100

    Add SpriteLoader

--- unity-project/Assets/Editor/CreatePrefabs.cs
+++ unity-project/Assets/Editor/CreatePrefabs.cs
@@ -1,175 +1,175 @@
-using UnityEngine;
-using UnityEditor;
-using UnityEditor.Animations;
-using System.IO;
+//using UnityEngine;
+//using UnityEditor;
+//using UnityEditor.Animations;
+//using System.IO;
 
-public class CreatePrefabs
-{
-    [MenuItem("Tools/Create Prefabs From Sprites")]
-    public static void CreatePrefabsFromSprites()
-    {
-        string spritesBasePath = "Assets/Sprites"; // Carpeta base de los sprites
-        string prefabsBasePath = "Assets/Prefabs"; // Carpeta base de los prefabs
+//public class CreatePrefabs
+//{
+//    [MenuItem("Tools/Create Prefabs From Sprites")]
+//    public static void CreatePrefabsFromSprites()
+//    {
+//        string spritesBasePath = "Assets/Sprites"; // Carpeta base de los sprites
+//        string prefabsBasePath = "Assets/Prefabs"; // Carpeta base de los prefabs
 
-        // Obtener todas las carpetas dentro de Sprites
-        string[] spriteDirectories = Directory.GetDirectories(spritesBasePath);
+//        // Obtener todas las carpetas dentro de Sprites
+//        string[] spriteDirectories = Directory.GetDirectories(spritesBasePath);
 
-        foreach (string spriteDir in spriteDirectories)
-        {
-            string folderName = Path.GetFileName(spriteDir);
-            string standardAnimsPath = Path.Combine("Assets", folderName, "standard"); // Ruta a las animaciones estándar
-            string customAnimsPath = Path.Combine("Assets", folderName, "custom"); // Ruta a las animaciones personalizadas
-            string prefabsPath = Path.Combine(prefabsBasePath, folderName);
+//        foreach (string spriteDir in spriteDirectories)
+//        {
+//            string folderName = Path.GetFileName(spriteDir);
+//            string standardAnimsPath = Path.Combine("Assets", folderName, "standard"); // Ruta a las animaciones estándar
+//            string customAnimsPath = Path.Combine("Assets", folderName, "custom"); // Ruta a las animaciones personalizadas
+//            string prefabsPath = Path.Combine(prefabsBasePath, folderName);
 
-            if (!Directory.Exists(prefabsPath))
-                Directory.CreateDirectory(prefabsPath);
+//            if (!Directory.Exists(prefabsPath))
+//                Directory.CreateDirectory(prefabsPath);
 
-            // Obtener las rutas de animación y asignarlas dependiendo de la carpeta
-            string idleClipPath = Path.Combine(customAnimsPath, "idle_custom.anim");
-            if (!File.Exists(idleClipPath))
-            {
-                idleClipPath = Path.Combine(standardAnimsPath, "idle_standard_Down.anim");
-            }
+//            // Obtener las rutas de animación y asignarlas dependiendo de la carpeta
+//            string idleClipPath = Path.Combine(customAnimsPath, "idle_custom.anim");
+//            if (!File.Exists(idleClipPath))
+//            {
+//                idleClipPath = Path.Combine(standardAnimsPath, "idle_standard_Down.anim");
+//            }
 
-            string walkUpClipPath = Path.Combine(customAnimsPath, "walk_Up.anim");
-            if (!File.Exists(walkUpClipPath))
-            {
-                walkUpClipPath = Path.Combine(standardAnimsPath, "walk_standard_Up.anim");
-            }
+//            string walkUpClipPath = Path.Combine(customAnimsPath, "walk_Up.anim");
+//            if (!File.Exists(walkUpClipPath))
+//            {
+//                walkUpClipPath = Path.Combine(standardAnimsPath, "walk_standard_Up.anim");
+//            }
 
-            string walkDownClipPath = Path.Combine(customAnimsPath, "walk_Down.anim");
-            if (!File.Exists(walkDownClipPath))
-            {
-                walkDownClipPath = Path.Combine(standardAnimsPath, "walk_standard_Down.anim");
-            }
+//            string walkDownClipPath = Path.Combine(customAnimsPath, "walk_Down.anim");
+//            if (!File.Exists(walkDownClipPath))
+//            {
+//                walkDownClipPath = Path.Combine(standardAnimsPath, "walk_standard_Down.anim");
+//            }
 
-            string walkLeftClipPath = Path.Combine(customAnimsPath, "walk_Left.anim");
-            if (!File.Exists(walkLeftClipPath))
-            {
-                walkLeftClipPath = Path.Combine(standardAnimsPath, "walk_standard_Left.anim");
-            }
+//            string walkLeftClipPath = Path.Combine(customAnimsPath, "walk_Left.anim");
+//            if (!File.Exists(walkLeftClipPath))
+//            {
+//                walkLeftClipPath = Path.Combine(standardAnimsPath, "walk_standard_Left.anim");
+//            }
 
-            string walkRightClipPath = Path.Combine(customAnimsPath, "walk_Right.anim");
-            if (!File.Exists(walkRightClipPath))
-            {
-                walkRightClipPath = Path.Combine(standardAnimsPath, "walk_standard_Right.anim");
-            }
+//            string walkRightClipPath = Path.Combine(customAnimsPath, "walk_Right.anim");
+//            if (!File.Exists(walkRightClipPath))
+//            {
+//                walkRightClipPath = Path.Combine(standardAnimsPath, "walk_standard_Right.anim");
+//            }
 
-            string attackUpClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackUp.anim");
-            if (!File.Exists(attackUpClipPath))
-            {
-                attackUpClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackUp.anim");
-            }
+//            string attackUpClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackUp.anim");
+//            if (!File.Exists(attackUpClipPath))
+//            {
+//                attackUpClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackUp.anim");
+//            }
 
-            string attackDownClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackDown.anim");
-            if (!File.Exists(attackDownClipPath))
-            {
-                attackDownClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackDown.anim");
-            }
+//            string attackDownClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackDown.anim");
+//            if (!File.Exists(attackDownClipPath))
+//            {
+//                attackDownClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackDown.anim");
+//            }
 
-            string attackLeftClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackLeft.anim");
-            if (!File.Exists(attackLeftClipPath))
-            {
-                attackLeftClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackLeft.anim");
-            }
+//            string attackLeftClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackLeft.anim");
+//            if (!File.Exists(attackLeftClipPath))
+//            {
+//                attackLeftClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackLeft.anim");
+//            }
 
-            string attackRightClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackRight.anim");
-            if (!File.Exists(attackRightClipPath))
-            {
-                attackRightClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackRight.anim");
-            }
+//            string attackRightClipPath = Path.Combine(customAnimsPath, sprite.name + "_AttackRight.anim");
+//            if (!File.Exists(attackRightClipPath))
+//            {
+//                attackRightClipPath = Path.Combine(standardAnimsPath, sprite.name + "_AttackRight.anim");
+//            }
 
-            // Ahora asignamos las rutas a las animaciones en el diccionario
-            var animationPaths = new System.Collections.Generic.Dictionary<string, string>
-            {
-                { "Idle", idleClipPath },
-                { "WalkUp", walkUpClipPath },
-                { "WalkDown", walkDownClipPath },
-                { "WalkLeft", walkLeftClipPath },
-                { "WalkRight", walkRightClipPath },
-                { "AttackUp", attackUpClipPath },
-                { "AttackDown", attackDownClipPath },
-                { "AttackLeft", attackLeftClipPath },
-                { "AttackRight", attackRightClipPath }
-            };
+//            // Ahora asignamos las rutas a las animaciones en el diccionario
+//            var animationPaths = new System.Collections.Generic.Dictionary<string, string>
+//            {
+//                { "Idle", idleClipPath },
+//                { "WalkUp", walkUpClipPath },
+//                { "WalkDown", walkDownClipPath },
+//                { "WalkLeft", walkLeftClipPath },
+//                { "WalkRight", walkRightClipPath },
+//                { "AttackUp", attackUpClipPath },
+//                { "AttackDown", attackDownClipPath },
+//                { "AttackLeft", attackLeftClipPath },
+//                { "AttackRight", attackRightClipPath }
+//            };
 
-            // Obtener todos los sprites en la carpeta actual
-            string[] spriteFiles = Directory.GetFiles(spriteDir, "*.png", SearchOption.AllDirectories);
+//            // Obtener todos los sprites en la carpeta actual
+//            string[] spriteFiles = Directory.GetFiles(spriteDir, "*.png", SearchOption.AllDirectories);
 
-            foreach (string spriteFile in spriteFiles)
-            {
-                string assetPath = spriteFile.Replace(Application.dataPath, "Assets");
-                Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+//            foreach (string spriteFile in spriteFiles)
+//            {
+//                string assetPath = spriteFile.Replace(Application.dataPath, "Assets");
+//                Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
 
-                if (sprite != null)
-                {
-                    // Crear GameObject
-                    GameObject newPrefab = new GameObject(sprite.name);
-                    SpriteRenderer spriteRenderer = newPrefab.AddComponent<SpriteRenderer>();
-                    spriteRenderer.sprite = sprite;
+//                if (sprite != null)
+//                {
+//                    // Crear GameObject
+//                    GameObject newPrefab = new GameObject(sprite.name);
+//                    SpriteRenderer spriteRenderer = newPrefab.AddComponent<SpriteRenderer>();
+//                    spriteRenderer.sprite = sprite;
 
-                    // Añadir Animator
-                    Animator animator = newPrefab.AddComponent<Animator>();
-                    string controllerPath = Path.Combine(standardAnimsPath, sprite.name + "_Controller.controller");
-                    AnimatorController animatorController = AnimatorController.CreateAnimatorControllerAtPath(controllerPath);
-                    animator.runtimeAnimatorController = animatorController;
+//                    // Añadir Animator
+//                    Animator animator = newPrefab.AddComponent<Animator>();
+//                    string controllerPath = Path.Combine(standardAnimsPath, sprite.name + "_Controller.controller");
+//                    AnimatorController animatorController = AnimatorController.CreateAnimatorControllerAtPath(controllerPath);
+//                    animator.runtimeAnimatorController = animatorController;
 
-                    // Crear estados y parámetros
-                    AnimatorStateMachine rootStateMachine = animatorController.layers[0].stateMachine;
-                    foreach (var entry in animationPaths)
-                    {
-                        string stateName = entry.Key;
-                        string standardClipPath = Path.Combine(standardAnimsPath, sprite.name + "_" + stateName + ".anim");
-                        string customClipPath = Path.Combine(customAnimsPath, sprite.name + "_" + stateName + ".anim");
-                        AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(customClipPath) ??
-                                            AssetDatabase.LoadAssetAtPath<AnimationClip>(standardClipPath);
+//                    // Crear estados y parámetros
+//                    AnimatorStateMachine rootStateMachine = animatorController.layers[0].stateMachine;
+//                    foreach (var entry in animationPaths)
+//                    {
+//                        string stateName = entry.Key;
+//                        string standardClipPath = Path.Combine(standardAnimsPath, sprite.name + "_" + stateName + ".anim");
+//                        string customClipPath = Path.Combine(customAnimsPath, sprite.name + "_" + stateName + ".anim");
+//                        AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(customClipPath) ??
+//                                            AssetDatabase.LoadAssetAtPath<AnimationClip>(standardClipPath);
 
-                        if (clip != null)
-                        {
-                            AnimatorState state = rootStateMachine.AddState(stateName);
-                            state.motion = clip;
-                        }
-                        else
-                        {
-                            Debug.LogWarning($"No se encontró la animación en Standard ni en Custom: {stateName} para {sprite.name}");
-                        }
-                    }
+//                        if (clip != null)
+//                        {
+//                            AnimatorState state = rootStateMachine.AddState(stateName);
+//                            state.motion = clip;
+//                        }
+//                        else
+//                        {
+//                            Debug.LogWarning($"No se encontró la animación en Standard ni en Custom: {stateName} para {sprite.name}");
+//                        }
+//                    }
 
-                    // Agregar transitions desde AnyState con triggers
-                    foreach (var entry in animationPaths)
-                    {
-                        AnimatorState targetState = FindState(rootStateMachine, entry.Key);
-                        if (targetState != null)
-                        {
-                            AnimatorStateTransition transition = rootStateMachine.AddAnyStateTransition(targetState);
-                            transition.AddCondition(AnimatorConditionMode.If, 0, entry.Key);
-                            transition.hasExitTime = false;
-                        }
-                    }
+//                    // Agregar transitions desde AnyState con triggers
+//                    foreach (var entry in animationPaths)
+//                    {
+//                        AnimatorState targetState = FindState(rootStateMachine, entry.Key);
+//                        if (targetState != null)
+//                        {
+//                            AnimatorStateTransition transition = rootStateMachine.AddAnyStateTransition(targetState);
+//                            transition.AddCondition(AnimatorConditionMode.If, 0, entry.Key);
+//                            transition.hasExitTime = false;
+//                        }
+//                    }
 
-                    // Guardar el GameObject como un prefab
-                    string prefabPath = Path.Combine(prefabsPath, sprite.name + ".prefab");
-                    PrefabUtility.SaveAsPrefabAsset(newPrefab, prefabPath);
+//                    // Guardar el GameObject como un prefab
+//                    string prefabPath = Path.Combine(prefabsPath, sprite.name + ".prefab");
+//                    PrefabUtility.SaveAsPrefabAsset(newPrefab, prefabPath);
 
-                    // Destruir GameObject temporal
-                    GameObject.DestroyImmediate(newPrefab);
-                }
-            }
-        }
+//                    // Destruir GameObject temporal
+//                    GameObject.DestroyImmediate(newPrefab);
+//                }
+//            }
+//        }
 
-        // Refrescar la base de datos de assets
-        AssetDatabase.Refresh();
-    }
+//        // Refrescar la base de datos de assets
+//        AssetDatabase.Refresh();
+//    }
 
-    private static AnimatorState FindState(AnimatorStateMachine stateMachine, string stateName)
-    {
-        foreach (var state in stateMachine.states)
-        {
-            if (state.state.name == stateName)
-            {
-                return state.state;
-            }
-        }
-        return null;
-    }
-}
+//    private static AnimatorState FindState(AnimatorStateMachine stateMachine, string stateName)
+//    {
+//        foreach (var state in stateMachine.states)
+//        {
+//            if (state.state.name == stateName)
+//            {
+//                return state.state;
+//            }
+//        }
+//        return null;
+//    }
+//}
---
+++ unity-project/Assets/Scripts/SpriteLoader.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+
+public class SpriteLoader : MonoBehaviour
+{
+    private string serverUrl = "http://localhost:4000/sprites"; // URL donde están los sprites
+
+    void Start()
+    {
+        Debug.Log("Iniciando descarga de AssetBundles...");
+        StartCoroutine(LoadSpritesFromServer()); // Inicia la descarga cuando el juego comienza
+    }
+
+    public IEnumerator LoadSpritesFromServer()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(serverUrl))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("❌ Error al obtener la lista de sprites: " + request.error);
+                yield break;
+            }
+
+            string[] spritePaths = JsonUtility.FromJson<SpriteList>(request.downloadHandler.text).sprites;
+
+            foreach (string path in spritePaths)
+            {
+                StartCoroutine(DownloadAndProcessSprite(path));
+            }
+        }
+    }
+
+    IEnumerator DownloadAndProcessSprite(string url)
+    {
+        using (UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url))
+        {
+            yield return request.SendWebRequest();
+
+            Debug.Log($"Descargando {url}");
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"❌ Error al descargar {url}: {request.error}");
+                yield break;
+            }
+
+            UnityEngine.AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+            if (bundle == null)
+            {
+                Debug.LogError($"⚠️ No se pudo cargar el AssetBundle desde {url}");
+                yield break;
+            }
+
+            string spriteName = Path.GetFileName(url);
+            (int cols, int rows) = GetSpriteGrid(spriteName);
+
+            ProcessSprite(bundle, spriteName, cols, rows);
+
+            bundle.Unload(false);
+        }
+    }
+
+    static (int cols, int rows) GetSpriteGrid(string fileName)
+    {
+        // Lógica para determinar columnas y filas dependiendo del archivo
+        if (fileName == "backslash_128.png" || fileName == "backslash.png") return (13, 4);
+        if (fileName == "halfslash_128.png" || fileName == "halfslash.png") return (6, 4);
+        if (fileName == "slash_128.png" || fileName == "slash_oversize.png" || fileName == "slash_reverse_oversize.png" || fileName == "slash.png") return (6, 4);
+        if (fileName == "thrust_oversize.png" || fileName == "thrust.png") return (8, 4);
+        if (fileName == "tool_rod.png") return (13, 4);
+        if (fileName == "tool_whip.png") return (8, 4);
+        if (fileName == "walk_128.png" || fileName == "walk.png") return (9, 4);
+        if (fileName == "wheelchair.png") return (2, 4);
+        if (fileName == "climb.png") return (6, 1);
+        if (fileName == "combat_idle.png") return (2, 4);
+        if (fileName == "emote.png") return (3, 4);
+        if (fileName == "hurt.png") return (1, 6);
+        if (fileName == "idle.png") return (2, 4);
+        if (fileName == "jump.png") return (5, 4);
+        if (fileName == "run.png") return (8, 4);
+        if (fileName == "shoot.png") return (13, 4);
+        if (fileName == "sit.png") return (3, 4);
+        if (fileName == "spellcast.png") return (7, 4);
+        return (1, 1); // Valor por defecto
+    }
+
+    void ProcessSprite(UnityEngine.AssetBundle bundle, string spriteName, int cols, int rows)
+    {
+        Sprite sprite = bundle.LoadAsset<Sprite>(spriteName);
+        if (sprite == null)
+        {
+            Debug.LogError($"❌ No se pudo cargar el sprite {spriteName}");
+            return;
+        }
+
+        Debug.Log($"✅ Procesando {spriteName} con {cols} columnas y {rows} filas");
+    }
+
+    [System.Serializable]
+    private class SpriteList
+    {
+        public string[] sprites;
+    }
+}

9450f738c0452db34801bba66f040513d32057d0
Climent Fernández <a23cliferand@inspedralbes.cat>
2025-04-02 22:14:08 +0200

    Update GridManager.cs

--- unity-project/Assets/Scripts/GridManager.cs
+++ unity-project/Assets/Scripts/GridManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.Networking;
 
 public class GridManager : MonoBehaviour
 {
@@ -65,56 +67,84 @@ public class GridManager : MonoBehaviour
 
     void StartCharacter(int x, int y, Character characterData, int army, int positionOnArray)
     {
-        if (_characterPrefab != null)
+        StartCoroutine(LoadCharacterPrefab(x, y, characterData, army, positionOnArray));
+    }
+
+    private IEnumerator LoadCharacterPrefab(int x, int y, Character characterData, int army, int positionOnArray)
+    {
+        Debug.Log($"info http://lordgrids.dam.inspedralbes.cat:4000/AssetBundles/{characterData.name}");
+        string assetBundleURL = $"http://lordgrids.dam.inspedralbes.cat:4000/AssetBundles/{characterData.name}";
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(assetBundleURL);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
         {
-            // Instantiate the character prefab at the specified position
-            var character = Instantiate(_characterPrefab, new Vector3(x * _tileSize, y * _tileSize, -1), Quaternion.identity);
-            character.name = characterData.name;
-
-            // Add the Character component and populate its properties
-            var characterComponent = character.AddComponent<Character>();
-            characterComponent.id = characterData.id;
-            characterComponent.name = characterData.name;
-            characterComponent.weapon = characterData.weapon;
-            characterComponent.vs_sword = characterData.vs_sword;
-            characterComponent.vs_spear = characterData.vs_spear;
-            characterComponent.vs_axe = characterData.vs_axe;
-            characterComponent.vs_bow = characterData.vs_bow;
-            characterComponent.vs_magic = characterData.vs_magic;
-            characterComponent.winged = characterData.winged;
-            characterComponent.sprite = characterData.sprite;
-            characterComponent.icon = "character_icon";
-            characterComponent.atk = characterData.atk;
-            characterComponent.movement = characterData.movement;
-            characterComponent.health = characterData.health;
-            characterComponent.actualHealth = characterData.actualHealth;
-            characterComponent.distance = characterData.distance;
-            characterComponent.hasMoved = characterData.hasMoved;
-
-            // Find the tile at the specified position and assign the character to it
-            var tile = GameObject.Find($"Tile {x} {y}");
-            if (tile != null)
-            {
-                var tileComponent = tile.GetComponent<Tile>();
-                tileComponent.Character = character;
-                if(army==1){
-                    characterComponent.internalId=positionOnArray;
-                } else{
-                    characterComponent.internalId=positionOnArray+4;
-                }
-                tileComponent.CharacterData = characterComponent;
-                tileComponent.isOccupied = true;
-
-                Debug.Log($"Character {characterData.name} created at {x}, {y}");
-            }
-            else
-            {
-                Debug.LogWarning($"Tile {x} {y} not found.");
-            }
+            Debug.LogError("Error al cargar AssetBundle: " + www.error);
         }
         else
         {
-            Debug.LogWarning("Character prefab is not assigned.");
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+            _characterPrefab = bundle.LoadAsset<GameObject>(characterData.name);
+
+            if (_characterPrefab != null)
+            {
+                Debug.Log("Character prefab cargado exitosamente.");
+
+                // Instantiate the character prefab at the specified position
+                var character = Instantiate(_characterPrefab, new Vector3(x * _tileSize, y * _tileSize, -1), Quaternion.identity);
+                character.name = characterData.name;
+
+                // Add the Character component and populate its properties
+                var characterComponent = character.AddComponent<Character>();
+                characterComponent.id = characterData.id;
+                characterComponent.name = characterData.name;
+                characterComponent.weapon = characterData.weapon;
+                characterComponent.vs_sword = characterData.vs_sword;
+                characterComponent.vs_spear = characterData.vs_spear;
+                characterComponent.vs_axe = characterData.vs_axe;
+                characterComponent.vs_bow = characterData.vs_bow;
+                characterComponent.vs_magic = characterData.vs_magic;
+                characterComponent.winged = characterData.winged;
+                characterComponent.sprite = characterData.sprite;
+                characterComponent.icon = characterData.icon;
+                characterComponent.atk = characterData.atk;
+                characterComponent.movement = characterData.movement;
+                characterComponent.health = characterData.health;
+                characterComponent.actualHealth = characterData.actualHealth;
+                characterComponent.distance = characterData.distance;
+                characterComponent.hasMoved = characterData.hasMoved;
+
+                // Find the tile at the specified position and assign the character to it
+                var tile = GameObject.Find($"Tile {x} {y}");
+                if (tile != null)
+                {
+                    var tileComponent = tile.GetComponent<Tile>();
+                    tileComponent.Character = character;
+                    if (army == 1)
+                    {
+                        characterComponent.internalId = positionOnArray;
+                    }
+                    else
+                    {
+                        characterComponent.internalId = positionOnArray + 4;
+                    }
+                    tileComponent.CharacterData = characterComponent;
+                    tileComponent.isOccupied = true;
+
+                    Debug.Log($"Character {characterData.name} created at {x}, {y}");
+                }
+                else
+                {
+                    Debug.LogWarning($"Tile {x} {y} not found.");
+                }
+            }
+            else
+            {
+                Debug.LogError("No se encontró el prefab CharacterTest en el AssetBundle.");
+            }
+
+            // Unload the AssetBundle to free memory
+            bundle.Unload(false);
         }
     }
 
